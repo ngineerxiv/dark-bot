b0f0f0ded7c6456754f4828c2b49a1e437a418a2
Arata Tanaka <tarata43@yahoo.co.jp>
2016-02-15 19:43:17 +0900

    Add httpd

---
+++ src/es2015/httpd.js
@@ -0,0 +1,11 @@
+let spawn = require('child_process').spawn
+
+module.exports = (robot) => {
+    robot.router.get("/hubot/version", (req, res) => {
+        res.end(robot.version);
+    })
+
+    robot.router.get("/hubot/ping", (req, res) => {
+        res.end("PONG")
+    });
+}
