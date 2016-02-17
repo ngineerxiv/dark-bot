a3cedfc66230928b0b12cbead736bbcdce145c86
Arata Tanaka <tarata43@yahoo.co.jp>
2016-02-17 23:19:11 +0900

    Add join script

--- src/es2015/join.js
+++ src/es2015/join.js
@@ -1,19 +1,40 @@
 // Description:
-//   新規参加者用挨拶スクリプト
+//   join all channels
 //
 
 var slackAPI = require('slackbotapi');
 var token = process.env.WEB_SLACK_TOKEN;
 var slack = new slackAPI({
     'token': token,
-    'logging': true,
     'autoReconnect': true
 });
 
 
 module.exports = (robot => {
     robot.respond(/join all/i, res => {
-
-        res.send('joined all channels')
+        let bot = slackAPI.getUser(robot.name);
+        slack.reqAPI("channels.list",{
+            exclude_archived: 1
+        }, (data) => {
+            if(!data.ok) {
+                robot.logger.error(`something ocuured ${data}`);
+                return;
+            }
+            data.channels
+                .filter((ch) => (ch.substring(0, 1) === 'C') )
+                .forEach((ch) => {
+                    slack.reqAPI("channels.invite", {
+                        channel: ch.id,
+                        user: bot.id
+                    }, (data) {
+                        if( data.ok ) {
+                            res.send(`joined #${ch.name}`)
+                        } else {
+                            robot.logger.error(`failed to join to ${ch}`);
+                            robot.logger.error(data);
+                        }
+                    });
+                });
+        });
     })
 })
