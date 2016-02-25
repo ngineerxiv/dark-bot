ec2d6d5f67fb84df1fcb0f75a31de62fddc3bd7a
Arata Tanaka <tarata43@yahoo.co.jp>
2016-02-25 12:55:30 +0900

    Add black list

--- src/es2015/zoi.js
+++ src/es2015/zoi.js
@@ -3,14 +3,26 @@
 //
 
 module.exports = (robot => {
+    const urataku = 'U034TCZKE';
     const targets = [1,1,1,1,1,2,3];
-    robot.respond(/zoi/i, res => {
-        let selected = res.random(targets);
+    let zoi = (res) => {
+        let filtered = targets;
+        try {
+            if(res.message.user.id === urataku) {
+                filtered = filtered.filter(elm => elm !== 1);
+            }
+        } catch (e) {
+            robot.logger.error(e);
+        }
+        let selected = res.random(filtered);
         res.send(`http://yamiga.waka.ru.com/images/zoi${selected}.jpg`);
+    };
+
+    robot.respond(/[zoi|ぞい]/i, res => {
+        zoi(res);
     })
 
     robot.hear(/.*[がんばる|頑張る]ぞい/i, res => {
-        let selected = res.random(targets);
-        res.send(`http://yamiga.waka.ru.com/images/zoi${selected}.jpg`);
+        zoi(res);
     });
 })
