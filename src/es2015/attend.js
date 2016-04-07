fdaacef1a47f6dbc23b046277d5c9fcb1110cc24
Arata Tanaka <tarata43@yahoo.co.jp>
2016-04-07 19:49:36 +0900

    Impl

--- src/es2015/attend.js
+++ src/es2015/attend.js
@@ -35,61 +35,73 @@ function updateEvents(url, logger, callback) {
     });
 }
 
-const load = function(robot) {
-    return robot.brain.get(HUBOT_ATTEND_KEY);
-};
-
-const save = function(robot, attendees) {
-    robot.brain.set(HUBOT_ATTEND_KEY, attendees);
-};
-
 
 module.exports = (robot => {
-    let events = load(robot);
-    let attendees = {};
+    let events = [];
 
-    const filterDuplicate = function(arr) {
-        return arr.filter((x, i, self) => self.indexOf(x) === i);
-    }
+    updateEvents(apiUrl, robot.logger, (updated) => {
+        events = updated;
+    });
 
-    const dateToKey = function(startAt) {
-        const d = new Date(startAt);
+    const load = function() {
+        const attendees = robot.brain.get(HUBOT_ATTEND_KEY)
+        return attendees ? attendees : {};
+    };
+
+    const save = function(attendees) {
+        robot.brain.set(HUBOT_ATTEND_KEY, attendees);
+    };
+
+    const dateToKey = function(e) {
+        const d = new Date(e.startAt);
         return `${d.getYear()}-${d.getMonth()}-${d.getDay()}`;
     }
 
-    const addAttendee = function(userName, startAt) {
-        const key = dateToKey(startAt);
-        let target = attendees[key];
-        target = target || [];
+    const addAttendee = function(userName, event) {
+        const attendees = load();
+        const key = dateToKey(event);
+        const target    = attendees[key] ? attendees[key] : [];
         const old = target;
         const changed = old.indexOf(userName) === -1;
         target.push(userName);
-        attendees[key] = filterDuplicate(target);
+        attendees[key] = target.filter((x, i, self) => self.indexOf(x) === i);
         return {
             after : attendees,
             changed: changed
         };
     };
 
-    const removeAttendee = function(userName, startAt) {
-        const key = dateToKey(startAt);
-        let target = attendees[key];
-        target = target || [];
-        const old = target;
-        const changed = old.indexOf(userName) !== -1
-        target = target.filter((x) => x !== userName);
-        attendees[key] = target;
+    const removeAttendee = function(userName, event) {
+        const attendees = load();
+        const key       = dateToKey(event);
+        const target    = attendees[key] ? attendees[key] : [];
+        const old       = target;
+        const changed   = old.indexOf(userName) !== -1
+        attendees[key] = target.filter((x) => x !== userName);
         return {
             after : attendees,
             changed: changed
         };
     };
 
-    const status = function() {
+    const key = () => {
+        const e = latest();
+        return e ? dateToKey(e) : null;
+    }
+
+    const latest = () => {
         if(events && events.length !== 0) {
-            const latest = events[0];
-            const start  = new Date(latest.startAt);
-            return `${latest.title} @ ${latest.location}\n 開始: ${start}`;
+            return events[0];
+        } else {
+            return null;
+        }
+    }
+
+    const status = () => {
+        const event = latest();
+        if ( event ) {
+            const start  = new Date(event.startAt);
+            return `${event.title} @ ${event.location}\n 開始: ${start}`;
         } else {
             return null;
         }
@@ -129,25 +141,30 @@ module.exports = (robot => {
             robot.logger.info("参加か不参加か不明なケース")
             robot.logger.info(tokens)
         } else if ( (isAttend && !notWord) || (notAttend && notWord) ) {
-            const result = addAttendee(userName);
+            const l = latest();
+            const result = addAttendee(userName, l);
             if(result.changed) {
                 res.send(`${userName} joined! \n${event}`);
-                save(robot, attendees);
+                save(result.after);
             }
-
-        } else {
-            const result = removeAttendee(userName);
+        } else if ( (isAttend && notWord) || (notAttend && !notWord) ) {
+            const l = latest();
+            const result = removeAttendee(userName, l);
             if(result.changed) {
                 res.send(`${userName} leaved! \n${event}`);
-                save(robot, attendees);
+                save(result.after);
             }
         }
     })
 
-    robot.respond(/mokumoku/i, res => {
-        const event = status();
+    robot.respond(/moku/i, res => {
+        const event     = status();
+        const attendees = load();
         if(event !== null) {
-            res.send(`直近のもくもく会: \n${event}`);
+            const k = key();
+            const list = attendees[k] ? attendees[k] : [];
+            const message = list.join(",");
+            res.send(`直近のもくもく会: \n${event}\n参加者: ${message}`);
         } else {
             res.send("直近のもくもく会は見当たらないなぁ・・・");
         }
