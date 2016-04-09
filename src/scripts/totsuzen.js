037ffccf3ebb8c1bad6aee929d9177f56c11b9e8
Arata Tanaka <tarata43@yahoo.co.jp>
2016-04-09 16:49:36 +0900

    Move totsuzen

---
+++ src/scripts/totsuzen.js
@@ -0,0 +1,15 @@
+const eastasianwidth = require('eastasianwidth');
+const strpad = (str, count) => new Array(count + 1).join(str)
+
+module.exports = (robot) => {
+    robot.respond(/突然の (.+)$/i, (msg) => {
+        const message = msg.match[1].replace(/^\s+|\s+$/g, '')
+        const length = Math.floor(eastasianwidth.length(message) / 2)
+        const suddendeath = [
+            `＿${strpad('人', length + 2)}＿`,
+            `＞　${message}　＜`,
+            `￣Y${strpad('^Y', length)}￣`
+        ];
+        msg.send(suddendeath.join("\n"))
+    })
+}
