531dd51587650f7051222760703aaef681acf2f3
go nakanishi <shiro.goma.goma@gmail.com>
2016-04-18 13:16:08 +0900

    知らなかったベンゼン

--- src/scripts/goma.js
+++ src/scripts/goma.js
@@ -7,7 +7,7 @@
 
 "use strict"
 
-const benzen = "```\n　　　CH \n　　　／＼ \n　　／　＼＼ \nHC／　　　＼＼CH \n ｜｜C6H6　　｜ \n ｜｜　´д｀｜ \nHC＼　　　／／CH \n　　＼　／／ \n　　　＼／ \n　　　CH\n```";
+const benzene = "```\n　　　CH \n　　　／＼ \n　　／　＼＼ \nHC／　　　＼＼CH \n ｜｜C6H6　　｜ \n ｜｜　´д｀｜ \nHC＼　　　／／CH \n　　＼　／／ \n　　　＼／ \n　　　CH\n```";
 const uuid = require("node-uuid");
 const gomas = [
     () => `http://yamiga.waka.ru.com/images/goma/01.jpg?cb=${uuid.v4()}`,
@@ -15,7 +15,7 @@ const gomas = [
     () => `http://yamiga.waka.ru.com/images/goma/03.jpg?cb=${uuid.v4()}`,
     () => `http://yamiga.waka.ru.com/images/goma/04.jpg?cb=${uuid.v4()}`,
     () => `http://yamiga.waka.ru.com/images/goma/05.jpg?cb=${uuid.v4()}`,
-    () => benzen
+    () => benzene
 ];
 module.exports = (robot) => {
     robot.respond(/goma$/i, (res) => {
