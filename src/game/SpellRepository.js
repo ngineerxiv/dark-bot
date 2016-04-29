855eb74e72127ec385481e70211e9179bbd91e3a
Arata Tanaka <tarata43@yahoo.co.jp>
2016-04-30 05:18:57 +0900

    Remove unused class

--- src/game/SpellRepository.js
+++ src/game/SpellRepository.js
@@ -1,38 +1,8 @@
 "use strict"
 
-const request = require("request");
 const Game      = require("node-quest");
 const Spell     = Game.Spell;
 const AttackEffect = Game.AttackEffect;
-
-function factoryAttackSpellFromObject(obj) {
-    let power = obj.power;
-    power = typeof power === "number" ? power : Infinity;
-    return new Spell(obj.name, obj.mp, new AttackEffect(power));
-}
-
-class SpellRepositoryOnHttp {
-    constructor(url) {
-        this.url = url;
-        this.spells = [];
-    }
-
-    update(callback, errorCallback) {
-        request(this.url, (err, res, body) => {
-            err && errorCallback(err);
-            try {
-                const json = JSON.parse(body);
-                this.spells = json.spells.attack.map((obj) => factoryAttackSpellFromObject(obj));
-            } catch (e) {
-                errorCallback(e)
-            }
-        });
-    };
-
-    get() {
-        return this.spells;
-    }
-}
 
 class SpellRepositoryOnMemory {
     constructor() {
