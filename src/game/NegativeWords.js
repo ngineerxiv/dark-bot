72e45842e97b59c1761b5a353e685f5144789a98
Arata Tanaka <tarata43@yahoo.co.jp>
2016-07-05 22:40:44 +0900

    Fix negative words service

--- src/game/NegativeWords.js
+++ src/game/NegativeWords.js
@@ -4,6 +4,7 @@ const negativeWordsRepository = new NegativeWordsRepository("http://yamiga.waka.
 const guessWords    = ['そう'];
 const denialWords   = ['ない'];
 const ableToGoHome  = ['帰れる', 'かえれる'];
+const appreciateWords = ['おつかれ', 'おつかれさま'];
 
 class NegativeWords {
     constructor(negativeWordsRepository, logger, defaultNegativeWords) {
@@ -20,6 +21,12 @@ class NegativeWords {
         )
     };
 
+    countPain(kuromojiFormedTokens) {
+        const negativeCount   = this.countNegativeWords(kuromojiFormedTokens.map((t) => t.basic_form));
+        const appreciateCount = this.countAppreciatingWords(kuromojiFormedTokens.map((t) => t.surface_form));
+        return Math.max(negativeCount - appreciateCount, 0);
+    }
+
     countNegativeWords(tokens) {
         this.updateNegativeWords();
         let negativeCount = 0;
@@ -34,11 +41,27 @@ class NegativeWords {
         return negativeCount;
     };
 
+    countAppreciatingWords(tokens) {
+        let appreciateCount = 0;
+        if( !Array.isArray(tokens) ) {
+            return appreciateCount;
+        }
+
+        tokens.forEach((token, idx) => {
+            const next = tokens[idx + 1] || "";
+            this.isAppreciatingWord(token, next) && appreciateCount++;
+        });
+        return appreciateCount;
+    }
+
     hasNegative(token, next) {
         return (
             this.negativeWords.indexOf(token) !== -1 &&
             (denialWords.indexOf(next) === -1 && guessWords.indexOf(next) === -1)
         ) || (ableToGoHome.indexOf(token) !== -1 && denialWords.indexOf(next) !== -1);
+    }
+    isAppreciatingWord(token, next) {
+        return appreciateWords.indexOf(token + next) !== -1;
     }
 
     static factory() {
