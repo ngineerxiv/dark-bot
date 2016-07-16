c17bedfd3f6d6bf3a6a4d32fb1adaa0503da6088
Arata Tanaka <tarata43@yahoo.co.jp>
2016-07-16 19:55:13 +0900

    Add spells

--- src/game/JobRepository.js
+++ src/game/JobRepository.js
@@ -18,18 +18,35 @@ class SpellRepositoryOnMemory {
                     new Spell("テオザケル", 100, new AttackEffect(900)),
                     new Spell("バオウザケルガ", 200, new AttackEffect(1800)),
                     new Spell("シンベルワンバオウザケルガ", 500, new AttackEffect(4500)),
-                    new Spell("シン・ベルワン・バオウ・ザケルガ", 500, new AttackEffect(4500))
+                    new Spell("シン・ベルワン・バオウ・ザケルガ", 500, new AttackEffect(4500)),
+                    new Spell("サイフォジオ", 120, new CureEffect(1000)),
+                    new Spell("レイス", 20, new AttackEffect(180)),
+                    new Spell("ギガノレイス", 50, new AttackEffect(450))
             ], new ParameterAdjust(500, 200, -100, 0)),
             new Job("死神", [
-                    new Spell("黒棺", 300, new AttackEffect(2500))
+                    new Spell("破道の一", 20, new AttackEffect(180)),
+                    new Spell("破道の四", 25, new AttackEffect(200)),
+                    new Spell("破道の三十三", 40, new AttackEffect(360)),
+                    new Spell("赤火砲", 40, new AttackEffect(360)),
+                    new Spell("破道の六十三", 100, new AttackEffect(1000)),
+                    new Spell("雷吼炮", 100, new AttackEffect(1000)),
+                    new Spell("破道の七十三", 120, new AttackEffect(1300)),
+                    new Spell("双蓮蒼火墜", 120, new AttackEffect(1300)),
+                    new Spell("破道の九〇", 300, new AttackEffect(2500)),
+                    new Spell("黒棺", 300, new AttackEffect(2500)),
+                    new Spell("破道の九十一", 250, new AttackEffect(2000)),
+                    new Spell("千手皎天汰炮", 220, new AttackEffect(2000)),
+                    new Spell("破道の九十六", 250, new AttackEffect(2200)),
+                    new Spell("一刀火葬", 250, new AttackEffect(2200))
             ], new ParameterAdjust(200, 0, 200, 30)),
             new Job("社畜", [
-                    new Spell("メラゾーマ", 20, new AttackEffect(175)),
-                    new Spell("メラガイアー", 50, new AttackEffect(450)),
+                    new Spell("メラゾーマ", 50, new AttackEffect(450)),
+                    new Spell("メラガイアー", 100, new AttackEffect(900)),
                     new Spell("アルテマ", 250, new AttackEffect(2200)),
                     new Spell("フレア", 200, new AttackEffect(1800)),
-                    new Spell("異議あり", 10, new AttackEffect(100)),
-            ], new ParameterAdjust(-200, 100, 500, 0)),
+                    new Spell("異議あり", 100, new AttackEffect(900)),
+                    new Spell("Redbull", 50, new CureEffect(500))
+            ], new ParameterAdjust(-200, 100, 700, 0)),
             new Job("光の戦士", [
                     new Spell("ケアルダ", 100, new CureEffect(900)),
                     new Spell("ケアルガ", 130, new CureEffect(1200)),
