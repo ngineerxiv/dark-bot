68d340de2839036829d1eb6c376e5e04fc2a6d93
Arata Tanaka <tarata43@yahoo.co.jp>
2016-07-21 12:37:19 +0900

    Add bitness ranking

--- views/game/js/bitness.js
+++ views/game/js/bitness.js
@@ -13,7 +13,7 @@
         req.send();
     };
 
-    class UserStatusTable extends React.Component {
+    class UserBitnessTable extends React.Component {
         constructor(props) {
             super(props);
             this.state = {
@@ -41,19 +41,9 @@
         render() {
             const rows = this.state.users.map((u) => {
                 return (
-                    <UserStatusRow
+                    <UserBitnessRow
                     name={u.name}
-                    hitPoint={u.hitPoint.current}
-                    maxHitPoint={u.hitPoint.max}
-                    magicPoint={u.magicPoint.current}
-                    maxMagicPoint={u.magicPoint.max}
-                    weaponAttack={u.equipment.weapon.averageOfAttack}
-                    weaponDivergence={u.equipment.weapon.divergenceOfAttack}
-                    paramAttack={u.parameter.attackPower}
-                    paramSkill={u.parameter.skillPoint}
-                    paramMind={u.parameter.mindPower}
-                    paramUnstability={u.parameter.mindStability}
-                    spells={u.spells.map((s) => s.name).join(",")}
+                    bitness={u.bitness}
                     />
                     );
             });
@@ -62,15 +52,7 @@
                     <thead>
                         <tr>
                             <th>名前</th>
-                            <th>HitPoint</th>
-                            <th>MagicPoint</th>
-                            <th>装備威力</th>
-                            <th>ばらつき</th>
-                            <th>基礎攻撃力</th>
-                            <th>技能</th>
-                            <th>精神力</th>
-                            <th>精神不安定さ</th>
-                            <th>使える魔法</th>
+                            <th>社会からうけたつらさ</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -81,27 +63,19 @@
         };
     };
 
-    class UserStatusRow extends React.Component {
+    class UserBitnessRow extends React.Component {
         render() {
             return (
                 <tr>
                     <th>{this.props.name}</th>
-                    <th>{this.props.hitPoint} / {this.props.maxHitPoint}</th>
-                    <th>{this.props.magicPoint} / {this.props.maxMagicPoint}</th>
-                    <th>{this.props.weaponAttack}</th>
-                    <th>{this.props.weaponDivergence}</th>
-                    <th>{this.props.paramAttack}</th>
-                    <th>{this.props.paramSkill}</th>
-                    <th>{this.props.paramMind}</th>
-                    <th>{this.props.paramUnstability}</th>
-                    <th>{this.props.spells}</th>
+                    <th>{this.props.bitness}</th>
                 </tr>
             );
         };
     };
 
     ReactDOM.render(
-        <UserStatusTable />,
+        <UserBitnessTable />,
         document.getElementById(renderDomId)
     );
 })("darkgame")
