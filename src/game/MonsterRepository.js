ff95b26a5ef4c7e324d8d608b36900fd15125dd2
Arata Tanaka <tarata43@yahoo.co.jp>
2016-08-16 15:10:50 +0900

    Add counters

--- src/game/MonsterRepository.js
+++ src/game/MonsterRepository.js
@@ -14,7 +14,11 @@ const Spell     = DarkQuest.Spell;
 const AttackEffect = DarkQuest.Effect.AttackEffect;
 const CureEffect   = DarkQuest.Effect.CureEffect;
 const StatusEffect = DarkQuest.Effect.StatusEffect;
+const MindAttackEffect = DarkQuest.Effect.MindAttackEffect;
 const StatusValues = DarkQuest.StatusValues;
+const Feedback = require("./spell/Feedback.js");
+const DrainFeedback = Feedback.DrainFeedback;
+const MagicDrainFeedback = Feedback.MagicDrainFeedback;
 
 class MonsterRepositoryOnMemory {
     constructor() {
@@ -22,7 +26,10 @@ class MonsterRepositoryOnMemory {
             new HitPoint(Infinity, Infinity), 
             new MagicPoint(Infinity, Infinity), 
             new Equipment(new Weapon("権力", 300, 120, new HitRate(95), new Critical(20))), 
-            new Parameter(100, 12)
+            new Parameter(100, 12),
+            [
+                new Spell("MPバスター", 0, new MindAttackEffect(Infinity, new MagicDrainFeedback()))
+            ]
         );
         const priest = new User(0, "神父", new HitPoint(Infinity, Infinity), new MagicPoint(Infinity, Infinity), new Equipment(new Weapon("慈悲", 0, 0, new HitRate(100))), new Parameter(800, 10), [
                 new Spell("レイズ", 20, [new StatusEffect(StatusValues.DEAD), new CureEffect(100)]),
@@ -30,10 +37,13 @@ class MonsterRepositoryOnMemory {
         ]);
         const holiday = new User(0, "休日", new HitPoint(Infinity, Infinity), new MagicPoint(Infinity, Infinity), new Equipment(new Weapon("", 0, 0, new HitRate(100))), new Parameter(Infinity, 0), [
             new Spell("アレイズ", 20, [new StatusEffect(StatusValues.DEAD), new CureEffect(Infinity)]),
-            new Spell("フルケア", 100, new CureEffect(Infinity))
+            new Spell("フルケア", 100, new CureEffect(Infinity)),
+            new Spell("MPバスター", 0, new MindAttackEffect(Infinity, new MagicDrainFeedback()))
         ]);
 
         priest.counter = new Spell("神の裁き", 0, new AttackEffect(Infinity));
+        world.counter = new Spell("MPバスター", 0, new MindAttackEffect(Infinity, new MagicDrainFeedback()));
+        holiday.counter = new Spell("MPバスター", 0, new MindAttackEffect(Infinity, new MagicDrainFeedback()));
         this.monsters = [world, priest, holiday];
     }
 
