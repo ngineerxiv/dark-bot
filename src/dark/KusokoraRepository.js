d0552d0dc71562a86ce993ca38029439e4ff8ec9
Arata Tanaka <tarata43@yahoo.co.jp>
2016-09-07 00:43:01 +0900

    Fix dark papix

---
+++ src/dark/KusokoraRepository.js
@@ -0,0 +1,16 @@
+class KusokoraRepositoryOnSqlite {
+    constructor(db) {
+        this.db = db;
+    }
+
+    getAll(callback) {
+        this.db.all("SELECT pictureUrl FROM kusokoras;", (err, rows) => {
+            if (err) {
+                return err;
+            }
+            return callback(rows.map((r) => r.pictureUrl));
+        });
+    }
+}
+
+module.exports = KusokoraRepositoryOnSqlite;
