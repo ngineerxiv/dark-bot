99034e93624617d425e1c06cda82574067086ff7
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2017-04-12 13:33:19 +0900

    ...

---
+++ src/scripts/kaeritai.js
@@ -0,0 +1,18 @@
+// Description:
+//   misc
+//
+// Commands:
+//   hubot poem - オサレポエム
+//   hubot 炎上 <text> <text> - ババーン
+//   なんだと・・・
+//   hubot goma - やればわかる
+//   hubot isao - やればわかる
+//
+
+"use strict"
+
+module.exports = (robot) => {
+    robot.hear(/.+/, () => {
+
+    })
+}
