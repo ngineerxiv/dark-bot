1ee7747bb1819023822b3a23abb8b829f019a0a0
Arata Tanaka <Arata_Tanaka@voyagegroup.com>
2017-04-21 20:32:40 +0900

    Impl

--- src/scripts/kaeritai.js
+++ src/scripts/kaeritai.js
@@ -11,8 +11,27 @@
 
 "use strict"
 
-module.exports = (robot) => {
-    robot.hear(/.+/, () => {
 
-    })
+const uuid = require("node-uuid");
+
+module.exports = (robot) => {
+    robot.hear(/すごい/, (res) => {
+        res.send(`https://pbs.twimg.com/media/C920YtzVwAAQZvX.jpg#${uuid.v4()}`);
+    });
+
+    robot.hear(/出勤/, (res) => {
+        res.send(`https://pbs.twimg.com/media/C920YtzVwAAQZvX.jpg#${uuid.v4()}`);
+    });
+
+    robot.hear(/帰った/, (res) => {
+        res.send(`https://pbs.twimg.com/media/C9I1iydVwAASPma.jpg#${uuid.v4()}`);
+    });
+
+    robot.hear(/遅刻/, (res) => {
+        res.send(`https://pbs.twimg.com/media/C9a7ilTVYAIBE3Q.jpg#${uuid.v4()}`);
+    });
+
+    robot.hear(/おはよう/, (res) => {
+        res.send(`https://pbs.twimg.com/media/C8mZoFoXoAMgt24.jpg#${uuid.v4()}`);
+    });
 }
