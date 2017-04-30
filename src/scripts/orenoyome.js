d45b4cffe20bebe8e969337eabfc48ac6d4a2c4e
Arata Tanaka <tarata43@yahoo.co.jp>
2017-04-30 23:59:58 +0900

    Fix...

--- src/scripts/orenoyome.js
+++ src/scripts/orenoyome.js
@@ -8,6 +8,8 @@
 //   hubot 流子|御船流子 - 御船流子.は俺の嫁.com
 //   hubot エリオ|藤和エリオ - 藤和エリオ.は俺の嫁.com
 
+"use strict"
+
 const punycode = require('punycode');
 const http  = require('request');
 const random = (max) => {
@@ -39,43 +41,51 @@ class OrenoyomeClient {
           }
       });
     }
-}
 
-function getUrlFromBody(body, yome) {
-    const resources = JSON.parse(body)
-    const max       = resources[yome]
-    const encoded   = `xn--${punycode.encode(yome)}`
-    const domain    = `${encoded}.xn--u9jb933vm9i.com`
-    return `http://${ domain }/images/${ random(max) }.jpg`
+    requestWith(yome, callback, errorCallback) {
+        return this.request((body) => {
+            const resources = JSON.parse(body)
+            const max       = resources[yome]
+            const encoded   = `xn--${punycode.encode(yome)}`
+            const domain    = `${encoded}.xn--u9jb933vm9i.com`
+            const url = `http://${ domain }/images/${ random(max) }.jpg`
+            callback(url);
+        }, errorCallback);
+    }
 }
 
 module.exports = (robot) => {
     const client = new OrenoyomeClient(
         orenoyomeMasterUrl
     );
-    robot.respond (/小路綾/i, (msg) => client.request(
-        (body) => msg.send(getUrlFromBody(body, '小路綾')),
-            (err, body) => msg.send(err)
+    robot.respond (/小路綾/i, (msg) => client.requestWith(
+        '小路綾',
+        (y) => msg.send(y),
+        (err, body) => msg.send(err)
     ));
 
-    robot.respond (/小野寺小咲/i, (msg) => client.request(
-        (body) => msg.send(getUrlFromBody(body, '小野寺小咲')),
-            (err, body) => msg.send(err)
+    robot.respond (/小野寺小咲/i, (msg) => client.requestWith(
+        '小野寺小咲',
+        (y) => msg.send(y),
+        (err, body) => msg.send(err)
     ));
 
-    robot.respond (/筒隠月子/i, (msg) => client.request(
-        (body) => msg.send(getUrlFromBody(body, '筒隠月子')),
-            (err, body) => msg.send(err)
+    robot.respond (/(月子|筒隠月子)/i, (msg) => client.requestWith(
+        '筒隠月子',
+        (y) => msg.send(y),
+        (err, body) => msg.send(err)
     ));
 
-    robot.respond (/(流子|御船流子)/i, (msg) => client.request(
-        (body) => msg.send(getUrlFromBody(body, '御船流子')),
-            (err, body) => msg.send(err)
+    robot.respond (/(流子|御船流子)/i, (msg) => client.requestWith(
+        '御船流子',
+        (y) => msg.send(y),
+        (err, body) => msg.send(err)
     ));
 
     robot.respond (/(エリオ|藤和エリオ)/i, (msg) => client.request(
-        (body) => msg.send(getUrlFromBody(body, '藤和エリオ')),
-            (err, body) => msg.send(err)
+        '藤和エリオ',
+        (y) => msg.send(y),
+        (err, body) => msg.send(err)
     ));
 
     robot.respond (/(orenoyome|俺の嫁) list/i, (msg) => {
