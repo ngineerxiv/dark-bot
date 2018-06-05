be520e164bdadce1455a575f4f8cb8b14e392b81
Arata Tanaka <tarata43@yahoo.co.jp>
2018-06-05 14:58:55 +0900

    :+1: Add sentry setting

--- src/scripts/_init.js
+++ src/scripts/_init.js
@@ -5,17 +5,30 @@
 
 "use strict";
 
+const config = require("config");
+const Raven = require('raven'); // TODO wrapper
+
 function isProduction() {
     return process.env.NODE_ENV === 'production';
+}
+
+function sentryIsSet() {
+    return config.sentry !== null;
 }
 
 module.exports = function(robot) {
     robot.logger.info(`node env: ${process.env.NODE_ENV}`);
     robot.logger.info(`server port: ${process.env.PORT}`);
+    if (sentryIsSet()) {
+        Raven.config(config.sentry).install();
+    }
 
     robot.error((err, res) => {
         robot.logger.error(`Error: ${err}`);
         robot.logger.error(`res: ${res}`);
         res && res.reply(err);
+        if (sentryIsSet()) {
+            Raven.captureException(err);
+        }
     });
 };
