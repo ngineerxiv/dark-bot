f42e02039bf3281dccab6ba8bb5853e3cb3ed809
Arata Tanaka <tarata43@yahoo.co.jp>
2018-06-05 14:55:02 +0900

    Add isProduction

--- src/scripts/_init.js
+++ src/scripts/_init.js
@@ -5,6 +5,10 @@
 
 "use strict";
 
+function isProduction() {
+    return process.env.NODE_ENV === 'production';
+}
+
 module.exports = function(robot) {
     robot.logger.info(`node env: ${process.env.NODE_ENV}`);
     robot.logger.info(`server port: ${process.env.PORT}`);
